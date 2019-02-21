34c9f19c0211c905cdbcd5c49caa7583b9b11494
csxiaodiao <i@xiaodiao.me>
2019-02-21 23:56:34 +0800

    fixed

--- lib/cat_ai.rb
+++ lib/cat_ai.rb
@@ -24,50 +24,54 @@ module CatAi
         yield(configuration)
       end
 
-      def create_params(mode, content, kw, src, dst)
-        body = {
-          wenzhang: content, 
-          v: mode, 
-          kw: kw, 
-          src: src, 
-          dst: dst, 
+      def create_params(**opt)
+        params = {
+          v: opt[:mode], 
+          kw: opt[:kw], 
+          src: opt[:src], 
+          dst: opt[:dst], 
+          count: opt[:count],
           key: configuration.app_key
         }.compact
 
-        body
+        params
+      end
+
+      def create_payload(content)
+        {wenzhang: content}
       end
 
       # model 1 smooth, v2 height, v3 highest
 
       def reedit(mode, content)
-        body =  create_params(mode, content).to_json
-        single_sender(body)
+        params =  {params: create_params(mode: mode)}
+        payload =  create_payload(content)
+
+        single_sender(params, payload)
       end
       
       # src original keywords
       # dst replace keywords
       # kw 1 list 2 add 3 delete
 
-      def keywords(kw, src, drc)
-        params = {params: create_params(kw, src, drc)}
+      def keywords(**opt)
+        params = {params: create_params(kw: opt[:kw], src: opt[:src], drc: opt[:drc])}
+
         get_single_sender(params)
       end
 
       def select(count)
-        params = {parmas: {count: count}}
+        params = {params: create_params(count: count)}
         get_single_sender(params)
       end
 
-      def get_single_sender(params)
-        header = {content_type: :json, accept: :json}
-        params = params.merge(header).to_json
+      def get_single_sender(params)    
         response = RestClient.get(configuration.post_url, params)
       end
 
 
-      def single_sender(body)
-        header = {content_type: :json, accept: :json}
-        response = RestClient.post(configuration.post_url, body, header)
+      def single_sender(params, payload)
+        response = RestClient.post(configuration.post_url, payload, params)
       end
 
 
